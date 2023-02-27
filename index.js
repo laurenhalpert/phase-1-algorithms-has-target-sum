c65d7efecd96cab43f485dfd6e62dce262411748
Lauren <halpertln@gmail.com>
2023-02-27 11:30:31 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  //Iterate over one elem's options
+  for (let i = 0; i< array.length; i++) {
+    //iterate over elem+1's options
+    for (let j = i +1; j< array.length; j++) {
+      if (array[i] + array[j] === target){
+        return true;
+      }
+      
+    }
+    
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  quadratic time O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  Iterate over one elem's options
+    i
+  iterate over elem+1's options
+    j
+    return arr[i] + arr[j] = target
+  return false
+  will be quadratic time
 */
 
 /*
   Add written explanation of your solution here
+
+  If a pair of numbers in the given array adds up to the target number, return true. If not, return false.
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +51,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([3, 6, 2, 7], 3));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([7, 10, 20, 9, 18], 16));
 }
 
 module.exports = hasTargetSum;
